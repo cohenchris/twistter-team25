b8e245a07cd504bb34441df51512daec49441325
Chris Cohen <officers@sigapp.club>
2019-09-30 17:32:33 -0400

    added imports for every page

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import StartPage from "./WebsitePages/StartPage/StartPage";
+//import AllTopics from "./WebsitePages/AllTopics";
+//import CreateMicroblog from "./WebsitePages/CreateMicroblog";
+//import CreateNewAccount from "./WebsitePages/CreateNewAccount";
+//import DownForMaintenance from "./WebsitePages/DownForMaintenance";
+//import HomePage from "./WebsitePages/HomePage";
+//import LoginPage from "./WebsitePages/LoginPage";
+//import ProfilePage from "./WebsitePages/ProfilePage";
+//import ProfileSettings from "./WebsitePages/ProfileSettings";
 
 export const blogDivStyle = {
   marginLeft: "200px",
