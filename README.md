3495263c302409ea8b6e653985c96819d6757754
Chris Cohen <officers@sigapp.club>
2019-10-19 17:54:35 -0400

    updated README

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+Front End ReactJS by Chris, Sabrina, and Cora.
+
+
+
 To start:
 
 git submodule init && git submodule update
--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-//import StartPage from "./WebsitePages/StartPage/StartPage";
-//import AllTopics from "./WebsitePages/AllTopics";
-//import CreateMicroblog from "./WebsitePages/CreateMicroblog";
-//import CreateNewAccount from "./WebsitePages/CreateNewAccount";
-//import DownForMaintenance from "./WebsitePages/DownForMaintenance";
-//import HomePage from "./WebsitePages/HomePage";
-//import LoginPage from "./WebsitePages/LoginPage";
+import StartPage from "./WebsitePages/StartPage/StartPage";
+import AllTopics from "./WebsitePages/AllTopics";
+import CreateMicroblog from "./WebsitePages/CreateMicroblog";
+import CreateNewAccount from "./WebsitePages/CreateNewAccount";
+import DownForMaintenance from "./WebsitePages/DownForMaintenance";
+import HomePage from "./WebsitePages/HomePage";
+import LoginPage from "./WebsitePages/LoginPage";
 import ProfilePage from "./WebsitePages/ProfilePage";
-//import ProfileSettings from "./WebsitePages/ProfileSettings";
+import ProfileSettings from "./WebsitePages/ProfileSettings";
 
 export const blogDivStyle = {
   marginLeft: "200px",
@@ -24,7 +24,7 @@ export const otherDivStyle = {
   marginRight: "50px"
 };
 
-ReactDOM.render(<ProfilePage />, document.getElementById("root"));
+ReactDOM.render(<StartPage />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
