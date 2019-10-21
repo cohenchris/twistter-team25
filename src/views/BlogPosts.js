237897e09711f49d2ed52ac5ac8ceb44abc021c2
Chris Cohen <officers@sigapp.club>
2019-10-21 17:50:01 -0400

    added like/quote counter and delete button for each post

--- src/views/BlogPosts.js
+++ src/views/BlogPosts.js
@@ -2,8 +2,7 @@
 
 import React, { useState } from "react";
 import { Container, Row, Col, Card, CardBody, Badge } from "shards-react";
-import ToggleButton from "react-bootstrap/ToggleButton";
-import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
+import { ToggleButton, ToggleButtonGroup, Button } from "react-bootstrap";
 
 export default class BlogPosts extends React.Component {
   constructor(props) {
@@ -18,7 +17,9 @@ export default class BlogPosts extends React.Component {
           title: "Conduct at an replied removal an amongst",
           body:
             "However venture pursuit he am mr cordial. Forming musical am hearing studied be luckily. But in for determine what would see...",
-          date: "28 February 2019"
+          date: "28 February 2019",
+          quotes: 24,
+          likes: 70
         },
         {
           category: "Travel",
@@ -27,7 +28,9 @@ export default class BlogPosts extends React.Component {
           title: "Off tears are day blind smile alone had ready",
           body:
             "Is at purse tried jokes china ready decay an. Small its shy way had woody downs power. To denoting admitted speaking learning my...fffffffffffffffffff",
-          date: "29 February 2019"
+          date: "29 February 2019",
+          quotes: 24,
+          likes: 70
         },
         {
           category: "Technology",
@@ -36,7 +39,9 @@ export default class BlogPosts extends React.Component {
           title: "Difficult in delivered extensive at direction",
           body:
             "Is at purse tried jokes china ready decay an. Small its shy way had woody downs power. To denoting admitted speaking learning my...",
-          date: "29 February 2019"
+          date: "29 February 2019",
+          quotes: 24,
+          likes: 70
         },
         {
           category: "Business",
@@ -45,7 +50,9 @@ export default class BlogPosts extends React.Component {
           title: "It so numerous if he may outlived disposal",
           body:
             "How but sons mrs lady when. Her especially are unpleasant out alteration continuing unreserved ready road market resolution...",
-          date: "29 February 2019"
+          date: "29 February 2019",
+          quotes: 24,
+          likes: 70
         },
         {
           category: "Travel",
@@ -54,7 +61,9 @@ export default class BlogPosts extends React.Component {
           title: "Attention he extremity unwilling on otherwise",
           body:
             "Conviction up partiality as delightful is discovered. Yet jennings resolved disposed exertion you off. Left did fond drew fat head poor jet pan flying over...",
-          date: "29 February 2019"
+          date: "29 February 2019",
+          quotes: 24,
+          likes: 70
         },
         {
           category: "Business",
@@ -63,7 +72,9 @@ export default class BlogPosts extends React.Component {
           title: "Totally words widow one downs few age every",
           body:
             "Discovered had get considered projection who favourable. Necessary up knowledge it tolerably. Unwilling departure education to admitted speaking...",
-          date: "29 February 2019"
+          date: "29 February 2019",
+          quotes: 24,
+          likes: 70
         },
         {
           category: "Computer Science",
@@ -71,7 +82,9 @@ export default class BlogPosts extends React.Component {
           title: "Had denoting properly jointure which",
           body:
             "In said to of poor full be post face snug. Introduced imprudence see say unpleasing devonshire acceptance son. Exeter longer wisdom work...",
-          date: "29 February 2019"
+          date: "29 February 2019",
+          quotes: 24,
+          likes: 70
         },
         {
           category: "Computer Science",
@@ -79,7 +92,9 @@ export default class BlogPosts extends React.Component {
           title: "Husbands ask repeated resolved but",
           body:
             "It abode words began enjoy years no do ﻿no. Tried spoil as heart visit blush or. Boy possible blessing sensible set but margaret interest. Off tears...",
-          date: "29 February 2019"
+          date: "29 February 2019",
+          quotes: 24,
+          likes: 70
         },
         {
           category: "Computer Science",
@@ -87,7 +102,9 @@ export default class BlogPosts extends React.Component {
           title: "Instantly gentleman contained belonging",
           body:
             "West room at sent if year. Numerous indulged distance old law you. Total state as merit court green decay he. Steepest merit checking railway...",
-          date: "29 February 2019"
+          date: "29 February 2019",
+          quotes: 24,
+          likes: 70
         },
         {
           category: "Computer Science",
@@ -97,7 +114,9 @@ export default class BlogPosts extends React.Component {
           title: "Extremity so attending objection as engrossed",
           body:
             "Pursuit chamber as elderly amongst on. Distant however warrant farther to of. My justice wishing prudent waiting in be...",
-          date: "29 February 2019"
+          date: "29 February 2019",
+          quotes: 24,
+          likes: 70
         },
         {
           category: "Computer Science",
@@ -107,7 +126,9 @@ export default class BlogPosts extends React.Component {
           title: "Bed sincerity yet therefore forfeited his",
           body:
             "Speaking throwing breeding betrayed children my to. Me marianne no he horrible produced ye. Sufficient unpleasing and...",
-          date: "29 February 2019"
+          date: "29 February 2019",
+          quotes: 24,
+          likes: 70
         },
         {
           category: "Computer Science",
@@ -117,7 +138,9 @@ export default class BlogPosts extends React.Component {
           title: "Object remark lively all did feebly excuse our",
           body:
             "Morning prudent removal an letters by. On could my in order never it. Or excited certain sixteen it to parties colonel not seeing...",
-          date: "29 February 2019"
+          date: "29 February 2019",
+          quotes: 24,
+          likes: 70
         },
         {
           category: "Computer Science",
@@ -127,7 +150,9 @@ export default class BlogPosts extends React.Component {
           title: "His followed carriage proposal entrance",
           body:
             "For county now sister engage had season better had waited. Occasional mrs interested far expression directly as regard...",
-          date: "29 February 2019"
+          date: "29 February 2019",
+          quotes: 24,
+          likes: 70
         }
       ]
     };
@@ -153,7 +178,12 @@ export default class BlogPosts extends React.Component {
                     >
                       {post.category}
                     </Badge>
+                    {/* TODO: make it so that this button only shows up when the post is created by the currently logged in user! */}
+                    <Button size="sm" variant="danger" className="float-right">
+                      X
+                    </Button>
                   </Col>
+
                   <Col>
                     <div className="card-post__author d-flex">
                       <a
@@ -174,7 +204,10 @@ export default class BlogPosts extends React.Component {
                 <CardBody>
                   <h5 className="card-title">{post.title}</h5>
                   <p className="card-text d-inline-block mb-3">{post.body}</p>
-                  <LikeAndQuoteButtons />
+                  <LikeAndQuoteButtons
+                    like_count={post.likes}
+                    quote_count={post.quotes}
+                  />
                 </CardBody>
               </Card>
             </Col>
@@ -185,23 +218,50 @@ export default class BlogPosts extends React.Component {
   }
 }
 
-export function LikeAndQuoteButtons() {
+const LikeAndQuoteButtons = props => {
   const [button, setButton] = useState();
-  const handleButtonPress = val => setButton(val);
+  const [likes, setLikes] = useState(props.like_count);
+  const [quotes, setQuotes] = useState(props.quote_count);
+  const handleLike = val => {
+    setButton(val);
+    if (val == 1) {
+      setLikes(likes + 1);
+    } else {
+      setLikes(likes - 1);
+    }
+  };
+  const handleQuote = val => {
+    setButton(val);
+    if (val == 2) {
+      setQuotes(quotes + 1);
+    } else {
+      setQuotes(quotes - 1);
+    }
+  };
 
   return (
-    <ToggleButtonGroup
-      type="checkbox"
-      size="sm"
-      value={button}
-      onChange={handleButtonPress}
-    >
-      <ToggleButton variant="outline-dark" value={1}>
-        Like
-      </ToggleButton>
-      <ToggleButton variant="outline-dark" value={2}>
-        Quote
-      </ToggleButton>
-    </ToggleButtonGroup>
+    <Row>
+      <Col>
+        <ToggleButtonGroup type="checkbox" size="sm" onChange={handleLike}>
+          <ToggleButton variant="outline-dark" value={1}>
+            Like
+          </ToggleButton>
+          <Button variant="dark" disabled>
+            {likes}
+          </Button>
+        </ToggleButtonGroup>
+      </Col>
+
+      <Col>
+        <ToggleButtonGroup type="checkbox" size="sm" onChange={handleQuote}>
+          <ToggleButton variant="outline-dark" value={2}>
+            Quote
+          </ToggleButton>
+          <Button variant="dark" disabled>
+            {quotes}
+          </Button>
+        </ToggleButtonGroup>
+      </Col>
+    </Row>
   );
-}
+};
