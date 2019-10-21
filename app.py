8c95f7df2eb7c356398730f66f8e8c602880b0f8
cornettn <noahcornett@gmail.com>
2019-10-21 17:16:59 -0400

    Formatting Changes

--- app.py
+++ app.py
@@ -8,198 +8,197 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def display_home_page():
-	return render_template('sample.html', **locals())
+    return render_template('sample.html', **locals())
 
 
 @app.route("/user-create-new-user")
 def create_user():
-	# Get the info form the frontend
-	username = "username"
-	commonName = "commonname"
-	email = "email"
-	phone = "111-111-1111"
-	birthday = "December 25, 0000"
-	description = "this is a description"
-	db.newUser(username, commonName, email, phone, birthday, description)
+    # Get the info form the frontend
+    username = "username"
+    commonName = "commonname"
+    email = "email"
+    phone = "111-111-1111"
+    birthday = "December 25, 0000"
+    description = "this is a description"
+    db.newUser(username, commonName, email, phone, birthday, description)
 
-	return render_template('sample.html', **locals())
+    return render_template('sample.html', **locals())
 
 
 @app.route("/user-update-common-name")
 def update_common_name():
-	# Get the info from the frontend
-	userId = 0  # Stored in the background of the front end
-	newCommonName = "new Common Name"
+    # Get the info from the frontend
+    userId = 0  # Stored in the background of the front end
+    newCommonName = "new Common Name"
 
-	db.updateCommonName(userId, newCommonName)
+    db.updateCommonName(userId, newCommonName)
 
-	return render_template('sample.html', **locals())
+    return render_template('sample.html', **locals())
 
 
 @app.route("/user-update-phone")
 def update_phone_number():
-	# Get the info from the database
-	userId = 0  # Stored in the background of the front end
-	newPhoneNumber = "222-222-2222"
+    # Get the info from the database
+    userId = 0  # Stored in the background of the front end
+    newPhoneNumber = "222-222-2222"
 
-	db.updatePhone(userId, newPhoneNumber)
+    db.updatePhone(userId, newPhoneNumber)
 
-	return render_template('sample.html', **locals())
+    return render_template('sample.html', **locals())
 
 
 @app.route("/user-update-description")
 def update_description():
-	# Get the info from the database
-	userId = 0  # Stored in the background of the front end
-	newDescription = "new description"
+    # Get the info from the database
+    userId = 0  # Stored in the background of the front end
+    newDescription = "new description"
 
-	db.updateDescription(userId, newDescription)
+    db.updateDescription(userId, newDescription)
 
-	return render_template('sample.html', **locals())
+    return render_template('sample.html', **locals())
 
 
 @app.route("/user-add-new-user-topic")
 def add_user_topic():
-	# Get the info from the database
-	userId = 0  # Stored in the background of the front end
-	newTopic = "Topic"
+    # Get the info from the database
+    userId = 0  # Stored in the background of the front end
+    newTopic = "Topic"
 
-	db.newUserTopic(userId, newTopic)
+    db.newUserTopic(userId, newTopic)
 
-	return render_template('sample.html', **locals())
+    return render_template('sample.html', **locals())
 
 
 @app.route("/user-get-user-timeline")
 def get_user_timeline():
-	# Get the info from the database
-	userId = 0  # The user ID of the timeline to show
+    # Get the info from the database
+    userId = 0  # The user ID of the timeline to show
 
-	db.getUserTimeline(userId)
+    db.getUserTimeline(userId)
 
-	return render_template('sample.html', **locals())
+    return render_template('sample.html', **locals())
 
 
 @app.route("/user-get-user-posts")
 def get_user_posts():
-	# Get the info from the database
-	userId = 0  # The user ID of the posts to show
+    # Get the info from the database
+    userId = 0  # The user ID of the posts to show
 
-	db.getUserPosts(userId)
+    db.getUserPosts(userId)
 
-	return render_template('sample.html', **locals())
+    return render_template('sample.html', **locals())
 
 
 @app.route("/user-delete")
 def delete_user():
-	# Get the info from the database
-	userId = 0  # The user ID to delete
+    # Get the info from the database
+    userId = 0  # The user ID to delete
 
-	db.deleteUser(userId)
+    db.deleteUser(userId)
 
-	return render_template('sample.html', **locals())
+    return render_template('sample.html', **locals())
 
 
 @app.route("/follow-user")
 def follow_new_user():
-	# Get the info from the database
-	userId = 0  # The user ID requesting a new follow
-	followingId = 1  # The id of the user who userId wants to follow
+    # Get the info from the database
+    userId = 0  # The user ID requesting a new follow
+    followingId = 1  # The id of the user who userId wants to follow
 
-	db.newFollow(userId, followingId)
+    db.newFollow(userId, followingId)
 
-	return render_template('sample.html', **locals())
+    return render_template('sample.html', **locals())
 
-# TODO: Add skeletons to the rest of the functions
 
 
 @app.route("/unfollow")
 def unfollow_user():
-	# Get the info from the database
-	userId = 0  # The user ID requesting a new follow
-	followingId = 1  # The id of the user who userId wants to unfollow
+    # Get the info from the database
+    userId = 0  # The user ID requesting a new follow
+    followingId = 1  # The id of the user who userId wants to unfollow
 
-	db.unfollowUser(userId, followingId)
+    db.unfollowUser(userId, followingId)
 
-	return render_template('sample.html', **locals())
+    return render_template('sample.html', **locals())
 
 
 @app.route("/follow-users-topics")
 def follow_user_topics():
-	# Get the info from the database
-	userId = 0  # The user ID requesting a new follow
-	followingId = 1  # The id of the user who userId wants to follow
-	topicsSelected = []
+    # Get the info from the database
+    userId = 0  # The user ID requesting a new follow
+    followingId = 1  # The id of the user who userId wants to follow
+    topicsSelected = []
 
-	db.updateFollow(userId, followingId, topics=topicsSelected)
+    db.updateFollow(userId, followingId, topics=topicsSelected)
 
-	return render_template('sample.html', **locals())
+    return render_template('sample.html', **locals())
 
 
 @app.route("/validate-email")
 def validate_email():
-	# Get the info from the database
-	email = "email"  # The user ID requesting a new follow
+    # Get the info from the database
+    email = "email"  # The user ID requesting a new follow
 
-	db.validateEmail(email)
+    db.validateEmail(email)
 
-	return render_template('sample.html', **locals())
+    return render_template('sample.html', **locals())
 
 
 @app.route("/validate-username")
 def validate_usernmae():
-	# Get info
-	username = "user"
-	db.validateUsername(username)
+    # Get info
+    username = "user"
+    db.validateUsername(username)
 
-	return render_template('sample.html', **locals());
+    return render_template('sample.html', **locals());
 
 
 @app.route("/get-user-id-from-email")
 def get_id_from_email():
-	email = "email"
-	db.getUserId(email)	
-	return render_template('sample.html', **locals())
+    email = "email"
+    db.getUserId(email)
+    return render_template('sample.html', **locals())
 
 
 @app.route("/create-post")
 def post():
-	userId = 0;
-	postText = "This is a post"
-	topics = "topics"
-	db.newPost(userId, postText, topics)
-	return render_template('sample.html', **locals())
+    userId = 0;
+    postText = "This is a post"
+    topics = "topics"
+    db.newPost(userId, postText, topics)
+    return render_template('sample.html', **locals())
 
 
 @app.route("/get-all-posts")
 def get_all_posts():
-	db.getAllPosts()
-	return render_template('sample.html', **locals())
+    db.getAllPosts()
+    return render_template('sample.html', **locals())
 
 
 @app.route("/get-topic-posts")
 def get_all_topic_posts():
-	topic = "topic"
-	db.getAllTopicPosts(topic)
-	return render_template('sample.html', **locals())
+    topic = "topic"
+    db.getAllTopicPosts(topic)
+    return render_template('sample.html', **locals())
 
 
 @app.route("/delete-post")
 def delete_post():
-	posttId = 0;
-	db.deletePost(postId)
-	return render_template('sample.html', **locals())
+    postId = 0;
+    db.deletePost(postId)
+    return render_template('sample.html', **locals())
 
 
 @app.route("/dm-user")
 def dm_user():
-	senderId = 0;
-	receiverId = 1;
-	message = "This is a message"
-	db.newDm(senderId, receiverId, message)
-	return render_template('sample.html', **locals())
+    senderId = 0;
+    receiverId = 1;
+    message = "This is a message"
+    db.newDm(senderId, receiverId, message)
+    return render_template('sample.html', **locals())
 
 
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
