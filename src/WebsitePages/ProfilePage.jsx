760a200da5429e793b7338aefa23ad6d84e58ba8
Chris Cohen <officers@sigapp.club>
2019-10-22 20:46:46 -0400

    added posts to user profile

--- src/WebsitePages/ProfilePage.jsx
+++ src/WebsitePages/ProfilePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavigationBar from "../components/NavigationBar";
 import UserDetails from "../components/user-profile-lite/UserDetails";
+import BlogPosts from "../views/BlogPosts";
 import { otherDivStyle } from "..";
 
 export default class ProfilePage extends React.Component {
@@ -10,6 +11,11 @@ export default class ProfilePage extends React.Component {
         <NavigationBar />
         <h1>PROFILE PAGE</h1>
         <UserDetails />
+        <br></br>
+        <br></br>
+        <br></br>
+        <h1>POSTS BY THIS USER</h1>
+        <BlogPosts />
       </div>
     );
   }
